67f5cb05b23e7dfbbc66ad8743ef257951725dfb
Vova <roysspirit@gmail.com>
2023-08-23 21:15:47 +0300

    topic page and remove display all prop on home page

--- src/Pages/Topics.jsx
+++ src/Pages/Topics.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import TopicElement from "../components/UI/TopicElement";
+import TopicList from "../components/topic/TopicList";
 
 const Topics = () => {
   return (
     <>
       <div className="mx-[16px] md:mx-[32px] lg:mx-[48px] my-10">
         <div className="mt-[60px]"></div>
-        {/* <TopicElement displayAll={true} /> */}
+        <TopicList />
       </div>
     </>
   );
